caa0eaf9d4dcbb5b83ab746717d1f9fa1d24b9fb
heroku20 <130670798+heroku20@users.noreply.github.com>
2023-05-15 14:58:04 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,179 @@
+ <?php
+echo '
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v5.8.9, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.8.9, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo5.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  
+  <title>LeanBiome Reviews</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel=\'stylesheet\'">
+  <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap"></noscript>
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+
+  
+  
+  
+</head>
+<body>
+  
+  <section data-bs-version="5.1" class="header11 cid-tDylk61qQX" id="header11-o">
+
+    
+
+    
+    
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6 image-wrapper">
+                <img class="w-100" src="assets/images/studio-project-removebg-preview-536x466.png" alt="Mobirise Website Builder">
+            </div>
+            <div class="col-12 col-md">
+                <div class="text-wrapper text-center">
+                    <h1 class="mbr-section-title mbr-fonts-style mb-3 display-1">
+                        <strong><u>LeanBiome™</u></strong><strong><br></strong></h1>
+                    <p class="mbr-text mbr-fonts-style display-7"><br>LeanBiome supplement is a probiotic supplement marketed as a weight loss and gut health supplement. It contains a proprietary blend of probiotic strains that are intended to support a healthy gut microbiome and improve digestive health.<br><br><br><strong><em>Special Offer Save Up to 70% OFF + Free Shipping!<br>Only for: $39/per bottle<br></em></strong><br></p>
+                    <div class="mbr-section-btn mt-3"><a class="btn btn-white display-7" href="https://bestofferhere.link/leanbiome">Visit Official Website Now<br></a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="info3 cid-tEjoAAyJbo" id="info3-q">
+
+    
+
+    
+    
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card col-12 col-lg-10">
+                <div class="card-wrapper">
+                    <div class="card-box align-center">
+                        <h4 class="card-title mbr-fonts-style align-center mb-4 display-2"><strong>Limited Time Special Pricing - Act Now!</strong></h4>
+                        <p class="mbr-text mbr-fonts-style mb-4 display-5">
+                            Secure Your Reserved LeanBiome While Stocks Last</p>
+                        <div class="mbr-section-btn mt-3"><a class="btn btn-black display-4" href="https://bestofferhere.link/leanbiome">&gt; Claim Your Massive Discount Now &lt;</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="pricing2 cid-tDybsEKil6" id="pricing2-n">
+    
+
+    
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6 align-center col-lg-4">
+                <div class="plan">
+                    <div class="plan-header">
+                        <h6 class="plan-title mbr-fonts-style mb-3 display-5">
+                            <strong>1 BOTTLE</strong></h6>
+                        <div class="plan-price">
+                            <p class="price mbr-fonts-style m-0 display-1"><strong>$59</strong></p>
+                            <p class="price-term mbr-fonts-style mb-3 display-7"><strong>PER BOTTLE</strong></p>
+                        </div>
+                    </div>
+                    <div class="plan-body">
+                        <div class="plan-list mb-4">
+                            <ul class="list-group mbr-fonts-style list-group-flush display-7">
+                                <li class="list-group-item">1-Month Supply</li><li class="list-group-item">Before Price: $129</li><li class="list-group-item"><span style="background-color: transparent; font-size: 1.2rem;">Now: $59.00</span></li><li class="list-group-item"><span style="background-color: transparent; font-size: 1.2rem;">You save $70.00</span><br></li><li class="list-group-item">180-DAY GUARANTEE</li><li class="list-group-item">+ SMALL SHIPPING</li>
+                            </ul>
+                        </div>
+                        <div class="mbr-section-btn text-center"><a href="https://bestofferhere.link/leanbiome" class="btn btn-danger display-4">ADD TO CART</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 align-center col-lg-4">
+                <div class="plan">
+                    <div class="plan-header">
+                        <h6 class="plan-title mbr-fonts-style mb-3 display-5">
+                            <strong>6 BOTTLE</strong></h6>
+                        <div class="plan-price">
+                            <p class="price mbr-fonts-style m-0 display-1"><strong>$39</strong></p>
+                            <p class="price-term mbr-fonts-style mb-3 display-7"><strong>PER BOTTLE</strong></p>
+                        </div>
+                    </div>
+                    <div class="plan-body">
+                        <div class="plan-list mb-4">
+                            <ul class="list-group mbr-fonts-style list-group-flush display-7">
+                                <li class="list-group-item">6-Month Supply + Bonus</li><li class="list-group-item">Before Price: $774</li><li class="list-group-item">Now: $234.00</li><li class="list-group-item">You save $540.00</li><li class="list-group-item">180-DAY GUARANTEE</li><li class="list-group-item">FREE SHIPPING</li>
+                            </ul>
+                        </div>
+                        <div class="mbr-section-btn text-center"><a href="https://bestofferhere.link/leanbiome" class="btn btn-danger display-4">ADD TO CART</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 align-center col-lg-4">
+                <div class="plan">
+                    <div class="plan-header">
+                        <h6 class="plan-title mbr-fonts-style mb-3 display-5">
+                            <strong>3 BOTTLE</strong></h6>
+                        <div class="plan-price">
+                            <p class="price mbr-fonts-style m-0 display-1"><strong>$49</strong></p>
+                            <p class="price-term mbr-fonts-style mb-3 display-7"><strong>PER BOTTLE</strong></p>
+                        </div>
+                    </div>
+                    <div class="plan-body">
+                        <div class="plan-list mb-4">
+                            <ul class="list-group mbr-fonts-style list-group-flush display-7">
+                                <li class="list-group-item">3-Month Supply + Bonus</li><li class="list-group-item">Before Price: $387</li><li class="list-group-item">Now: $147</li><li class="list-group-item">You save $240.00</li><li class="list-group-item">180-DAY GUARANTEE</li><li class="list-group-item">+ SMALL SHIPPING</li>
+                            </ul>
+                        </div>
+                        <div class="mbr-section-btn text-center"><a href="https://bestofferhere.link/leanbiome" class="btn btn-danger display-4">ADD TO CART</a></div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="footer3 cid-s48P1Icc8J" once="footers" id="footer3-i">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="row row-links">
+                <ul class="foot-menu">
+                    
+                    
+                    
+                    
+                    
+                <li class="foot-menu-item mbr-fonts-style display-7"><a href="" class="text-danger">Terms &amp; Conditions</a></li><li class="foot-menu-item mbr-fonts-style display-7"><a href="" class="text-danger">Privacy</a></li><li class="foot-menu-item mbr-fonts-style display-7"><a href="" class="text-danger">Disclaimer&nbsp;</a></li></ul>
+            </div>
+            
+            <div class="row row-copirayt">
+                <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">COPYRIGHT © LeanBiome - ALL RIGHTS RESERVED.</p>
+            </div>
+        </div>
+    </div>
+  
+  
+</body>
+</html>
+';
+?>
